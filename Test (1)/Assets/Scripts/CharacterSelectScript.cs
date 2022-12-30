e0e9f53bd3a1056da418ecdf153719364c452690
TobiDzN <reemaroes111@gmail.com>
2022-12-30 22:50:40 +0200

    loading screen BABY

--- Test (1)/Assets/Scripts/CharacterSelectScript.cs
+++ Test (1)/Assets/Scripts/CharacterSelectScript.cs
@@ -1,46 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CharacterSelectScript : MonoBehaviour
 {
 
     public static bool clicked = false;
     public static string whatClicked;
-
+    [SerializeField] private GameObject characterScreen;
+    [SerializeField] private GameObject loaderCanvas;
+    [SerializeField] private Image loader;
     private void Awake()
     {
         
     }
 
+    public async void LoadScene()
+    {
+        var scene = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        scene.allowSceneActivation = false;
+
+        characterScreen.SetActive(false);
+        loaderCanvas.SetActive(true);
+
+        do
+        {
+            await Task.Delay(100);
+            loader.fillAmount = scene.progress;
+
+        } while (scene.progress < 0.9f);
+
+        scene.allowSceneActivation = true;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
     public void MedicButton()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        LoadScene();
         clicked = true;
         whatClicked = "medic";
     }
     public void TDButton()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        LoadScene();
         clicked = true;
         whatClicked = "td";
     }
     public void TankButton()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        LoadScene();
         clicked = true;
         whatClicked = "tank";
     }
     public void MageButton()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        LoadScene();
         clicked = true;
         whatClicked = "mage";
     }
     public void RandomButton()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        LoadScene();
         clicked = true;
         whatClicked = "random";
     }
